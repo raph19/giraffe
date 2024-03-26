555e11b015866a64f7e3a1c34a2199794d0c54f9
raph19 <119010079+raph19@users.noreply.github.com>
2024-03-26 16:39:39 +0200

    Update team.js

--- app/models/team.js
+++ app/models/team.js
@@ -4,8 +4,9 @@ var mongoose = require("mongoose");
 var teamSchema = mongoose.Schema({
     team_name:{type:Object},
     admin_id:{type:Object},
-    member_ids:[{type:Object}]
-
+    member_ids:[{type:Object}],
+    projects:[],
+    posts:[]
 });
 
 var Team = mongoose.model("Team", teamSchema);
