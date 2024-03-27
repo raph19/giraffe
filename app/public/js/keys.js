f5fffce674e75e7f270b51dbc78b3c49f1be0a92
raph19 <119010079+raph19@users.noreply.github.com>
2024-03-27 16:52:54 +0200

    Update keys.js

--- app/public/js/keys.js
+++ app/public/js/keys.js
@@ -8,17 +8,17 @@ export function keys(){
 let togl=true;             
 var newobj;
 window.addEventListener('keydown', function (event) {
-switch (event.key.toLowerCase()) {
-    case 't':
+switch (event.key) {
+    case 'T':
             if (event.shiftKey) {
         transformControls.setMode('translate')
         break}
-    case 'r':
+    case 'R':
         if (event.shiftKey) {
         if (obj.userData.merged === undefined)
         transformControls.setMode('rotate')
         break}
-    case 's':
+    case 'S':
         if (event.shiftKey) {
         if (obj.userData.merged === undefined)
         transformControls.setMode('scale')
@@ -41,10 +41,10 @@ switch (event.key.toLowerCase()) {
         case 'y':if (event.ctrlKey) {
             editorHistory.redo();
             break}
-        case 'c': if(event.shiftKey) {
+        case 'C': if(event.shiftKey) {
          cloning();
             break}
-            case'd': if(event.shiftKey) {
+            case'D': if(event.shiftKey) {
             removeobj();
             break}
                 /*case'`':
